15c8e9fd6d13a5d1a2f73b2c6dc11c6d5c5bf432
yashu931 <yashwanthgowda388@gmail.com>
2025-09-16 06:22:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import pandas as pd
 import pickle
@@ -69,4 +68,5 @@ if prediction_proba > 0.5:
     st.write('The customer is likely to churn.')
 else:
     st.write('The customer is not likely to churn.')
+
 
